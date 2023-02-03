5c047c93dc6351944f88e9e3994350be06404b8e
ilker turan <ilkerturan@outlook.com>
2023-02-03 15:39:50 +0300

    03.02.2023 15:40

--- UI.MVC/Program.cs
+++ UI.MVC/Program.cs
@@ -102,6 +102,11 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
