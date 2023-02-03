c8b4ad9b1dbfd02a55470ace926ae31be7e9c15c
ilker turan <ilkerturan@outlook.com>
2023-02-03 09:54:51 +0300

    03.02.2023 10:00

--- UI.MVC/Controllers/PersonController.cs
+++ UI.MVC/Controllers/PersonController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Services.Services;
 using UI.MVC.Models;
 using UI.MVC.ViewHelpers;
-
 namespace UI.MVC.Controllers
 {
     public class PersonController : Controller
@@ -12,11 +11,13 @@ namespace UI.MVC.Controllers
         private IPersonService _service;
         private IDepartmentService _dService;
         private IPersonHelper _helper;
-        public PersonController(IPersonService service, IPersonHelper helper, IDepartmentService dService)
+        private IWebHostEnvironment _environment;
+        public PersonController(IPersonService service, IPersonHelper helper, IDepartmentService dService, IWebHostEnvironment environment)
         {
             _service = service;
             _helper = helper;
             _dService = dService;
+            _environment = environment;
         }
         public IActionResult Index()
         {
@@ -35,8 +36,19 @@ namespace UI.MVC.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(CreatePersonViewModel model)
+        public IActionResult Create(CreatePersonViewModel model, IFormFile ProfilePicture)
         {
+            if (ProfilePicture != null && ProfilePicture.Length > 0)
+            {
+                string fName = Guid.NewGuid().ToString() + Path.GetExtension(ProfilePicture.FileName);
+                var upload_path = Path.Combine(_environment.WebRootPath, "uploads", fName);
+
+                using (var stream = new FileStream(upload_path,FileMode.Create))
+                {
+                    ProfilePicture.CopyTo(stream);
+                }
+            }
+
             //var c = ModelState["NewPerson"].Children.Any(x => x.ValidationState != Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Valid);
             ModelState.Remove("Departments");
             if (ModelState.IsValid)
@@ -52,7 +64,7 @@ namespace UI.MVC.Controllers
 
         public bool ToggleActive(int id)
         {
-           return _service.ToggleActive(id);
+            return _service.ToggleActive(id);
         }
 
         public bool ToggleDeleted(int id)
